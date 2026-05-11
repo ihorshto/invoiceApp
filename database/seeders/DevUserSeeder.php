c65221fd0d5d006759bdd01826ede3e1c9d80b4a
Ihor Shtohryn <ihor@ecaste.com>
2026-05-11 22:58:03 +0200

    feat(seed): add DevUserSeeder with user and company for local dev

---
+++ database/seeders/DevUserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DevUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $user = User::firstOrCreate(
+            ['email' => 'dev@invoiceapp.test'],
+            [
+                'name'     => 'Dev User',
+                'locale'   => 'fr',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        if (! $user->company) {
+            Company::create([
+                'user_id'     => $user->id,
+                'name'        => 'Ma Société SARL',
+                'address'     => '12 rue de la Paix',
+                'postal_code' => '75001',
+                'city'        => 'Paris',
+                'country'     => 'France',
+                'email'       => 'contact@masociete.fr',
+                'phone'       => '+33 1 23 45 67 89',
+                'vat_number'  => 'FR12345678901',
+            ]);
+        }
+    }
+}
